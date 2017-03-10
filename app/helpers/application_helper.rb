0e7244bff77e174534320e5e6dfaa3cf04b045aa
m-lamarre <studium.square.pantz@gmail.com>
2017-03-10 17:31:52 +0200

    updated layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title(page_title = '')
-    base_title = "Hypertube"
+    base_title = "HyperTube"
     if page_title.empty?
       base_title
     else
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'movies#index'
+
+  get    '/sign_in',  to: 'sessions#new'
+  post   '/sign_in',  to: 'sessions#create'
+
+  delete '/sign_out', to: 'sessions#destroy'
+
+  get    '/sign_up',  to: 'registrations#new'
+  get    '/cancel',   to: 'registrations#cancel'
+  get    '/edit',     to: 'registrations#edit'
 end
