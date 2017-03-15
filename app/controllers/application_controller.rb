0b81db534b6c0ea74a0d4cf30c0fe81b172aa3c2
m-lamarre <studium.square.pantz@gmail.com>
2017-03-15 10:55:08 +0200

    added users controller test and updated controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,14 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_sign_up_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
+      added_attrs = [:username, :first_name, :last_name, :email, :password, :password_confirmation]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     end
 
     def configure_permitted_sign_in_parameters
       devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-        user_params.permit(:username, :email)
+        user_params.permit(:username, :email, :password)
       end
     end
 end
