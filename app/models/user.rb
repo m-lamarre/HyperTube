e06b7410c916b6273ab055d8a485bf66e2107612
m-lamarre <studium.square.pantz@gmail.com>
2017-03-15 10:22:48 +0200

    added username, first_name & last_name to user model and db

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+
+  def login=(login)
+   @login = login
+  end
+
+  def login
+   @login || self.username || self.email
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307151438) do
+ActiveRecord::Schema.define(version: 20170314142822) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -33,8 +33,12 @@ ActiveRecord::Schema.define(version: 20170307151438) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
