511b10844d7a089ddb6cfdb274722c3fa371dfbc
m-lamarre <studium.square.pantz@gmail.com>
2017-03-15 17:54:48 +0200

    updated sign-in & sign-out functions

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -20,6 +20,6 @@ before_action :configure_sign_in_params, only: [:create]
 
     # If you have extra params to permit, append them to the sanitizer.
     def configure_sign_in_params
-      devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email, :password])
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
     end
 end
