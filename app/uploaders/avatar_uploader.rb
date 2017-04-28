2a2111043e035403d4b9a954539a06b2df3060ed
Kalilo Jade Hansmann <kalilohansmann@gmail.com>
2017-04-28 15:44:59 +0200

    Added svg support

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,12 +1,9 @@
 class AvatarUploader < ApplicationUploader
 
-  google_login ENV['gmail_username']
-  google_password ENV['gmail_password']
-
   version(:avatar) { process resize_to_fill: [200, 200] }
 
   def extension_white_list
-    %w/jpg jpeg png/
+    %w/jpg jpeg png svg/
   end
 
 end
