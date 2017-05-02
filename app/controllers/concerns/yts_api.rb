abe72e72c16e381fb4335e7520e1e438e57c7557
Kalilo Jade Hansmann <kalilohansmann@gmail.com>
2017-05-02 14:48:43 +0200

    Wrote functions to use yts api

---
+++ app/controllers/concerns/yts_api.rb
@@ -0,0 +1,38 @@
+module YtsApi
+  include HTTParty
+  
+private
+
+  def serialize_yts_torrent(torrent)
+    t = Hash.new ''
+    t[:url] = torrent['url']
+    t[:hash] = torrent['hash']
+    t[:quality] = torrent['quality']
+    t[:size] = torrent['size_bytes']
+    t[:seeds] = torrent['seeds']
+    t[:peers] = torrent['peers']
+    t
+  end
+
+  def serialize_yts_movie(movie)
+    m = Hash.new ''
+    m[:title] = movie['title']
+    m[:year] = movie['year']
+    m[:rating] = movie['rating'] / 2
+    m[:runtime] = movie['runtime']
+    m[:genres] = movie['genres']
+    m[:description] = movie['description_full']
+    m[:image_url] = movie['medium_cover_image']
+    m[:torrents] = movie['torrents'].map { |torrent| serialize_yts_torrent(torrent) }
+    m
+  end
+
+  def serialize_yts_movies(movies)
+    movies.map { |movie| serialize_yts_movie(movie) }.reduce
+  end
+
+  def movies_from_yts(amount = 20, page = 1)
+    api_data = HTTParty.get("https://yts.ag/api/v2/list_movies.json?limit=#{amount}&page=#{page}")
+    api_data['status'] == 'ok' ? serialize_yts_movies(api_data['data']['movies']) : api_data['status_message']
+  end
+end
