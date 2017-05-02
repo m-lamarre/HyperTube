65b133b8ee68e8f4b947c78f0880f3e56192e60b
Keang Nage <knage@student.42.fr>
2017-05-02 11:23:44 +0200

    got 42 oauth working

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,6 +11,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
+  def marvin
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: '42') if is_navigational_format?
+    else
+      redirect_to new_user_registration_url
+    end
+  end
+
   # GET|POST /resource/auth/twitter
   # def passthru
   #   super
