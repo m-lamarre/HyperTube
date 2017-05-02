0d0650e8582b32c1c2c219cebadc7427632d34fb
Kalilo Jade Hansmann <kalilohansmann@gmail.com>
2017-05-02 16:50:18 +0200

    Added in identyfication keys

--- app/controllers/concerns/yts_api.rb
+++ app/controllers/concerns/yts_api.rb
@@ -18,6 +18,8 @@ private
   def serialize_yts_movie(movie)
     m = Hash.new ''
     m[:title] = movie['title']
+    m[:id] = movie['id']
+    m[:source] = 'yts'
     m[:year] = movie['year']
     m[:rating] = movie['rating'] / 2
     m[:runtime] = movie['runtime']
@@ -33,7 +35,7 @@ private
   end
 
   def get_yts_movie_by_id(id = 0)
-    apidata = HTTParty.get("https://yts.ag/api/v2/movie_details.json?movie_id=#{id}")
+    api_data = HTTParty.get("https://yts.ag/api/v2/movie_details.json?movie_id=#{id}")
     api_data['status'] == 'ok' ? serialize_yts_movie(api_data['data']['movie']) : api_data['status_message']
   end
 
