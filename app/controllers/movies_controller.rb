a7a371a8cc5535745bc7ea6f8fddc4b497b8d7ec
Kalilo Jade Hansmann <kalilohansmann@gmail.com>
2017-05-02 14:53:51 +0200

    Extracted shared content to homepage_controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,2 @@
-class MoviesController < ApplicationController
-  include YtsApi
-  include RandomFunctions
-
-  @random_movies
-
-  def index
-    @random_movies ||= select_random_movies
-  end
+class MoviesController < HomepagesController
 end
