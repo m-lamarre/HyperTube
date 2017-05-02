ef6acd70a4172c3dfd4829126d33e2c7083f6125
Kalilo Jade Hansmann <kalilohansmann@gmail.com>
2017-05-02 16:49:23 +0200

    Added show controller for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,12 @@
 class MoviesController < HomepagesController
+  before_action :authenticate_user!
+
+  def show
+    get_movie_by_source
+  end
+
+private
+  def get_movie_by_source
+    @movie ||= get_yts_movie_by_id(params[:id]) if params[:source] == 'yts'
+  end
 end
