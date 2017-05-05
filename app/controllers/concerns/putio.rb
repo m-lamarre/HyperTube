c8ff1e4571b333190b4a59ab91de9896440b9d5e
Kalilo Jade Hansmann <kalilohansmann@gmail.com>
2017-05-05 10:33:48 +0200

    Added more methods

--- app/controllers/concerns/putio.rb
+++ app/controllers/concerns/putio.rb
@@ -1,14 +1,25 @@
 class Putio
 
   def self.list
-    JSON.parse RestClient.get('https://api.put.io/v2/files/list', default_query)
+    JSON.parse RestClient.get('https://api.put.io/v2/files/list', default_query) rescue { error: 'failed to list files' }
   end
 
   def self.upload(url='https://yts.ag/torrent/download/B1671B938EB021B2B4505ECAD8789C6B4C30624C')
     query = default_query[:params]
     query[:file] = open(url)
 
-    RestClient.post('https://upload.put.io/v2/files/upload', query)
+    JSON.parse RestClient.post('https://upload.put.io/v2/files/upload', query) rescue { error: 'failed to upload' }
+  end
+
+  def self.get_file_by_id id
+    JSON.parse RestClient.get("https://api.put.io/v2/files/#{id}", default_query) rescue { error: 'file not found' }
+  end
+
+  def self.delete_file_by_id id
+    query = default_query[:params]
+    query[:file_ids] = id.to_s
+
+    JSON.parse RestClient.post("https://api.put.io/v2/files/delete", query) rescue { error: 'failed to delete file' }
   end
 
   private
