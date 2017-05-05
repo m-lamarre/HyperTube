01f020272d0c80eb3a8519a82cb4b807c46d731f
Kalilo Jade Hansmann <kalilohansmann@gmail.com>
2017-05-05 13:45:19 +0200

    Added more functions and renamed functions

--- app/controllers/concerns/putio.rb
+++ app/controllers/concerns/putio.rb
@@ -4,22 +4,45 @@ class Putio
     JSON.parse RestClient.get('https://api.put.io/v2/files/list', default_query) rescue { error: 'failed to list files' }
   end
 
-  def self.upload(url='https://yts.ag/torrent/download/B1671B938EB021B2B4505ECAD8789C6B4C30624C')
+  def self.upload(url)
     query = default_query[:params]
-    query[:file] = open(url)
+    query[:url] = url
+    query[:callback_url] = nil
 
-    JSON.parse RestClient.post('https://upload.put.io/v2/files/upload', query) rescue { error: 'failed to upload' }
+    JSON.parse RestClient.post('https://api.put.io/v2/transfers/add', query) rescue { error: 'failed to upload' }
   end
 
-  def self.get_file_by_id id
+  def self.get_file(id)
     JSON.parse RestClient.get("https://api.put.io/v2/files/#{id}", default_query) rescue { error: 'file not found' }
   end
 
-  def self.delete_file_by_id id
+  def self.delete_file(id)
     query = default_query[:params]
     query[:file_ids] = id.to_s
 
-    JSON.parse RestClient.post("https://api.put.io/v2/files/delete", query) rescue { error: 'failed to delete file' }
+    JSON.parse RestClient.post('https://api.put.io/v2/files/delete', query) rescue { error: 'failed to delete file' }
+  end
+
+  def self.get_torrent_status(torrent_id)
+    JSON.parse RestClient.get("https://api.put.io/v2/transfers/#{torrent_id}", default_query)
+  end
+
+  def self.share(id)
+    query = default_query[:params]
+    query[:file_ids] = id.to_s
+    query[:friends] = 'everyone'
+
+    JSON.parse RestClient.post('https://api.put.io/v2/files/share', query)
+  end
+
+  def self.download(id)
+    RestClient.get("https://api.put.io/v2/files/#{id}/download", default_query) do |response, request, result, &block|
+      if [301, 302, 307].include? response.code
+        redirected_url = response.headers[:location]
+      else
+        error: 'file not found'
+      end
+    end
   end
 
   private
