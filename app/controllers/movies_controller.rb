224b05d5d7bbb7671d0f5a0801d13f7e16987ca9
Kalilo Jade Hansmann <kalilohansmann@gmail.com>
2017-05-09 13:40:54 +0200

    Added functions to save the movie to the database

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,13 +11,39 @@ class MoviesController < HomepagesController
   end
 
   def play
+    get_movie
   end
 
 private
 
-def get_movie_by_source
-  @movie ||= get_yts_movie_by_id(params[:id]) if params[:source] == 'yts'
-  @movie ||= select_random_movies(1).first
-end
+  def get_movie_by_source
+    @movie ||= get_yts_movie_by_id(params[:id]) if params[:source] == 'yts'
+    @movie ||= select_random_movies(1).first
+  end
+
+  def find_and_save_movie
+    movie_from_source = get_yts_movie_by_id(params[:id]) if params[:source] == 'yts'
+    movie = save_movie_to_database
+  end
+
+  def save_movie_to_database(movie_from_source)
+    torrent = movie_from_source[:torrents].select { |m| m[:quality] == params[:quality] }.first
+
+    Movie.create(
+      title: movie_from_source[:title],
+      source: movie_from_source[:source],
+      movie_id: movie_from_source[:id],
+      quality: params[:quality],
+      size: torrent[:size],
+      url: torrent[:url],
+      stored: false,
+      stored_at: 0
+    )
+  end
+
+  def get_movie
+    @movie = Movie.find_by(params[:source], params[:id], params[:quality])
+    @movie ||= find_and_save_movie
+  end
 
 end
