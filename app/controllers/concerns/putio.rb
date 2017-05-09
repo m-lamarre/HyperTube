8b8d699d9d23f1c701c6ca4edf006c5b9d8562b1
Kalilo Jade Hansmann <kalilohansmann@gmail.com>
2017-05-09 15:42:10 +0200

    Updated rescue

--- app/controllers/concerns/putio.rb
+++ app/controllers/concerns/putio.rb
@@ -43,9 +43,9 @@ class Putio
       if [301, 302, 307].include? response.code
         response.headers[:location]
       else
-        { error: 'file not found. Can\'t download folders' }
+        { error: 'Can\'t download folders' }
       end
-    end
+    end rescue { error: 'file not found.' }
   end
 
   def self.find_id_of_movie_in_folder(id)
